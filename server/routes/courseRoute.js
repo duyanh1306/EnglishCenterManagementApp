a4333826bc8db673d20e96e81e5aeea2c45549cd
noobsmassacre <duyanh13062004@gmail.com>
2025-07-05 23:10:39 +0700

    Add courseRoute.js with complete CRUD routes

---
+++ server/routes/courseRoute.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const authAdmin = require("../middlewares/authAdmin");
+const {getAllCourses, createCourse, updateCourse, deleteCourse  } = require("../controllers/courseController");
+
+router.get("/", getAllCourses);
+router.post("/add",authAdmin, createCourse);
+router.put("/update/:id",authAdmin, updateCourse);
+router.delete("/delete/:id",authAdmin, deleteCourse);
+module.exports = router;
