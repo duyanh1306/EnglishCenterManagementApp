b2fd9ad8dc69e10b939c3fb10ccac7e9d4c4d1c0
noobsmassacre <duyanh13062004@gmail.com>
2025-07-10 23:13:58 +0700

    Small fix

--- server/routes/teacherRoute.js
+++ server/routes/teacherRoute.js
@@ -5,6 +5,6 @@ const authAdmin = require("../middlewares/authAdmin");
 const { getTeacherSchedule } = require("../controllers/teacherController");
 
 // Get teaching schedule for a specific teacher
-router.get("/:teacherId/schedule", authAdmin, getTeacherSchedule);
+router.get("/:teacherId/schedule", authTeacher, getTeacherSchedule);
 
 module.exports = router; 
