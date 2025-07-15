93302f7d911f1aaca6fd9ff033541630fc7bae27
kattasenpai31 <thangquoc10052020@gmail.com>
2025-07-15 21:47:18 +0700

    fix UserSchema and comment Teaching Schedule cuz delete file getDecodedToken

--- server/models/User.js
+++ server/models/User.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
+  userName: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   roleId: { type: String, required: true },
-  phone: { type: String },
+  birthday: { type: String },
+  number: { type: String },
   createdAt: { type: Date, default: Date.now },
   isActive: { type: Boolean, default: true },
 });
