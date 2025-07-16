9690de0d9175534d4e99149fa15a1e531c46e068
noobsmassacre <duyanh13062004@gmail.com>
2025-07-16 17:21:39 +0700

    fix: update User.js before merging with newLuong

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-<<<<<<< HEAD
 const userSchema = new mongoose.Schema(
   {
     fullName: {
@@ -43,18 +42,5 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-=======
-const userSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-  userName: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  roleId: { type: String, required: true },
-  birthday: { type: Date, required: true },
-  number: { type: String },
-  createdAt: { type: Date, default: Date.now },
-  isActive: { type: Boolean, default: true },
-});
->>>>>>> d328f3944ed457119c90ecb66dd0847355c1dae6
 
 module.exports = mongoose.model("User", userSchema);
