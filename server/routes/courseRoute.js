eb611fa7b195b721a432ed8f30aef7a492d5ab61
noobsmassacre <duyanh13062004@gmail.com>
2025-07-16 17:21:02 +0700

    fix: update courseRoute before merging with newLuong

--- server/routes/courseRoute.js
+++ server/routes/courseRoute.js
@@ -9,14 +9,8 @@ const {
   deleteCourse,
 } = require("../controllers/courseController");
 
-<<<<<<< HEAD
-router.get("/", authAdmin, getAllCourses);
-router.post("/add",  createCourse);
-router.put("/update/:id",  updateCourse);
-=======
 router.get("/", getAllCourses);
 router.post("/add", createCourse);
 router.put("/update/:id", updateCourse);
->>>>>>> d328f3944ed457119c90ecb66dd0847355c1dae6
 router.delete("/delete/:id", deleteCourse);
 module.exports = router;
